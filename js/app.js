7f5bd59e30be5b42475a34b4c0ffc6c116724d95
KeNeJ2004 <58132107+KeNeJ2004@users.noreply.github.com>
2020-06-23 17:48:19 +0300

    правки

--- js/app.js
+++ js/app.js
@@ -1,11 +1,15 @@
 var mailLink = document.querySelector(".mail-button");
 var modalPopup = document.querySelector(".modal-window");
 var loginClose = modalPopup.querySelector(".modal-close");
-var submit = modalPopup.querySelector(".button-submit")
+var submit = modalPopup.querySelector(".button-submit");
+
+
+
 
 mailLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalPopup.classList.add("modal-show");
+  nameform.focus();
 });
 
 loginClose.addEventListener("click", function (evt) {
@@ -14,8 +18,19 @@ loginClose.addEventListener("click", function (evt) {
 });
 
 submit.addEventListener("click", function (evt) {
-  evt.preventDefault();  
+  evt.preventDefault();
 });
+
+window.addEventListener("keydown", function (evt) {
+  if (evt.keyCode === 27) {
+    if (modalPopup.classList.contains("modal-show")) {
+      evt.preventDefault();
+      modalPopup.classList.remove("modal-show");
+    }
+  }
+});
+
+
 
 
 
