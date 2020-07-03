0011b4e1ce647d0aaeb37241745e8af5f6b0ebc0
KeNeJ2004 <58132107+KeNeJ2004@users.noreply.github.com>
2020-07-03 22:16:43 +0300

    правки финиш

--- js/script.js
+++ js/script.js
@@ -1,23 +1,45 @@
 var mailLink = document.querySelector(".mail-button");
 var modalPopup = document.querySelector(".modal-window");
-var loginClose = modalPopup.querySelector(".modal-close");
+var modalClose = modalPopup.querySelector(".modal-close");
 var submit = modalPopup.querySelector(".button-submit");
+var nameForm = modalPopup.querySelector(".name-form")
+var mailForms = modalPopup.querySelector(".mail-forms");
+var emailForm = modalPopup.querySelector(".email-form");
+var mailTextarea = modalPopup.querySelector(".mail-textarea");
 
 
+var isStorageSupport = true;
+var storage = "";
+
+try {
+  storage = localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
 
 mailLink.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalPopup.classList.add("modal-show");
-  nameform.focus();
+
+  if (storage) {
+    emailForm.value = storage;
+  }
+
+  nameForm.focus();
 });
 
-loginClose.addEventListener("click", function (evt) {
+modalClose.addEventListener("click", function (evt) {
   evt.preventDefault();
   modalPopup.classList.remove("modal-show");
+  modalPopup.classList.remove("modal-error");
 });
 
-submit.addEventListener("click", function (evt) {
-  evt.preventDefault();
+mailForms.addEventListener("submit", function (evt) {
+  if (!nameForm.value || !emailForm.value || !mailTextarea.value ) {
+    evt.preventDefault();
+  } else {
+    localStorage.setItem("login", emailForm.value);
+  }
 });
 
 window.addEventListener("keydown", function (evt) {
@@ -25,9 +47,18 @@ window.addEventListener("keydown", function (evt) {
     if (modalPopup.classList.contains("modal-show")) {
       evt.preventDefault();
       modalPopup.classList.remove("modal-show");
+      modalPopup.classList.remove("modal-error");
     }
   }
 });
+
+mailForms.addEventListener("submit", function (evt) {
+  if (!nameForm.value || !emailForm.value || !mailTextarea.value ) {
+    evt.preventDefault();
+    modalPopup.classList.add("modal-error");
+  }
+});
+
 
 
 var slidefirst = document.querySelector(".slide-first");
